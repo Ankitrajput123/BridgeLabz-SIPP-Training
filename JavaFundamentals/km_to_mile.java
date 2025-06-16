5c22c4f648aac009161673a914ffd97dcaa12f75
Ankitrajput123 <ankitthakur33172@gmail.com>
2025-06-16 13:29:37 +0530

    [Ankit].Add Problems for Java fundamentals on Day2

---
+++ JavaFundamentals/km_to_mile.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class km_to_mile{
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in); 
+double km= sc.nextDouble();
+double conversion= km/1.6;
+System.out.print(conversion);
+}
+}
