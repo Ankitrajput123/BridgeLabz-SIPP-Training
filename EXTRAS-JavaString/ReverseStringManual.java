1c3458e717c2b4a26685e90f9b2b6cc885efba1a
Ankitrajput123 <ankitthakur33172@gmail.com>
2025-06-22 17:58:12 +0530

    [Ankit].Add Problems for EXTRAS-JavaString on Day8

---
+++ EXTRAS-JavaString/ReverseStringManual.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ReverseStringManual {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+                System.out.print("Enter a string to reverse: ");
+        String input = scanner.nextLine();
+
+                String reversed = reverseString(input);
+
+        // Print the result
+        System.out.println("Reversed string: " + reversed);
+    }
+
+        public static String reverseString(String str) {
+        String result = "";
+
+        
+        for (int i = str.length() - 1; i >= 0; i--) {
+            result += str.charAt(i);
+        }
+
+        return result;
+    }
+}
